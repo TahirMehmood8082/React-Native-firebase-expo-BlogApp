82d67326f2e7b572435019e25963cff8af68de9e
TahirMehmood8082 <infinitidev31aug22@gmail.com>
2023-10-14 15:16:24 +0500

    access by process.env instead import

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,28 +3,19 @@ import { getAuth } from 'firebase/auth';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from '@firebase/firestore'
-import {
-  REACT_APP_API_KEY,
-  REACT_APP_AUTH_DOMAIN,
-  REACT_APP_DATABASE_URL,
-  REACT_APP_PROJECT_ID,
-  REACT_APP_STORAGE_BUCKET,
-  REACT_APP_MESSAGING_SENDER_ID,
-  REACT_APP_APP_ID,
-  REACT_APP_MEASUREMENT_ID,
-} from "@env"
+
 
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  databaseURL: REACT_APP_DATABASE_URL,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
